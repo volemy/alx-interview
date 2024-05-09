d481be30ae171011513ddbb9593017bd813a78ac
volemy <minayobenevolence0@gamil.com>
2024-05-09 14:59:46 +0300

    task 0/updates

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -5,27 +5,33 @@ operations in file: Copy, All and Paste.
 """
 
 
-def minOperations(n):
+def min_operations(n):
     """
-    Calculate the fewest number of operations needed to result in exactly n H characters in the file.
+    Determine the minimum number of operations
+    needed to result in exactly n 'H' characters.
 
-    Args:
-    n (int): The target number of H characters.
+    Parameters:
+    n (int): The target number of 'H' characters.
 
     Returns:
-    int: The minimum number of operations needed. If n is impossible to achieve, return 0.
+    int: The minimum number of operations required,
+    or 0 if n cannot be achieved.
     """
     if n < 2:
         return 0
 
     operations = 0
-    current_chars = 2
+    factor = 2
 
     while n > 1:
-        # If current_chars is a factor of n, we can reach n by copying and pasting
-        if n % current_chars == 0:
-            operations += current_chars
-            n //= current_chars
-            current_chars += 1
+        while n % factor == 0:
+            operations += factor
+            n //= factor
+        factor += 1
 
     return operations
+
+
+if __name__ == "__main__":
+    print("Min # of operations to reach {} char: {}".format(4, min_operations(4)))
+    print("Min # of operations to reach {} char: {}".format(12, min_operations(12)))
